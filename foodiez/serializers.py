58358d895afade022af42f7249b789a0552bb170
Waleedx7 <w.alshelahi@gmail.com>
2023-03-05 20:01:21 +0300

    first_comit

---
+++ foodiez/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Category, Ingredient, Recipe, Rating
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "first_name", "last_name"]
+
+    def create(self, validated_data):
+        username = validated_data["username"]
+        password = validated_data["password"]
+        first_name = validated_data["first_name"]
+        last_name = validated_data["last_name"]
+        new_user = User(username=username, first_name=first_name, last_name=last_name)
+        new_user.set_password(password)
+        new_user.save()
+        return validated_data
+    
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+class RecipeSerializer(serializers.ModelSerializer):
+    chef = serializers.ReadOnlyField(source='chef.username')
+    category = CategorySerializer()
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
--- foodiezproject/settings.py
+++ foodiezproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodiez',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
